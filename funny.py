f0930f43a9259ee8aae6d660cb1e0d82db456fba
ewr43qt54e4wtg <206362359+ewr43qt54e4wtg@users.noreply.github.com>
2025-04-06 13:37:56 -0400

    funny.py

---
+++ funny.py
@@ -0,0 +1,8 @@
+funny = input("wirte something funny")
+if funny == "i":{
+    print("you are not funny")
+}
+else:
+    print(funny+". haha")
+if funny == "you are funny":
+    print("you are not funny")
